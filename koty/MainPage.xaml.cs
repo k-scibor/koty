5a5046ec2cc44ea5c48ea63796e1e6365a611527
k-scibor <karolina.scibor1@wp.pl>
2024-11-26 21:00:59 +0100

    :3

---
+++ koty/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+namespace koty
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() {
+            this.InitializeComponent();
+        }
+        private void generuj(object sender, EventArgs e)
+        {
+            kot.Source = "https://cataas.com/cat";
+        }
+    }
+    
+}
---
+++ koty/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace koty
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
