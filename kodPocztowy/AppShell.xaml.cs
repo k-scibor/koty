beb3d18e97f498a65d47b11d4bd2e2911a188791
k-scibor <karolina.scibor1@wp.pl>
2024-11-26 19:40:43 +0100

    chyba działa??

---
+++ kodPocztowy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kodPocztowy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kodPocztowy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace kodPocztowy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
